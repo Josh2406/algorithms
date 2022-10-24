aab7c1deabde78a050a41a861a86ccf48e00a2ce
Josh2406 <ajibade.joshua.aj@gmail.com>
2022-10-24 13:13:15 +0100

    Adds SquaresOfSortedArray algorithm

---
+++ Algorithms.Implementations/AlgoExpert/Easy/SqauresOfSortedArray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms.Implementations.AlgoExpert.Easy
+{
+    public static class SqauresOfSortedArray
+    {
+        /// <summary>
+        /// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number 
+        /// sorted in non-decreasing order. <br/><br/>
+        /// <b>Example</b>: <br/>
+        /// <code>
+        /// Input: nums = [-4,-1,0,3,10]
+        /// Output: [0,1,9,16,100]
+        /// </code>
+        /// Explanation: After squaring, the array becomes[16, 1, 0, 9, 100].<br></br>
+        /// After sorting, it becomes[0, 1, 9, 16, 100].
+        /// </summary>
+        /// <param name="_"></param>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int[] SortedSquares(this Algorithm _, int[] nums)
+        {
+            var length = nums.Length;
+            var result = Enumerable.Repeat(0, length).ToArray();
+            int smallestElementIdx = 0;
+            int largestElementIdx = length - 1;
+
+            for(int i = length - 1; i >= 0; i--)
+            {
+                var smallestElement = nums[smallestElementIdx];
+                var largestElement = nums[largestElementIdx];
+
+                if(Math.Abs(smallestElement) >= Math.Abs(largestElement))
+                {
+                    result[i] = smallestElement * smallestElement;
+                    smallestElementIdx++;
+                }
+                else
+                {
+                    result[i] = largestElement * largestElement;
+                    largestElementIdx--;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- Algorithms.Implementations/AlgoExpert/Easy/TwoNumberSum.cs
+++ Algorithms.Implementations/AlgoExpert/Easy/TwoNumberSum.cs
@@ -9,7 +9,13 @@ namespace Algorithms.Implementations.AlgoExpert.Easy
         /// Given an array of integers <b><i>nums</i></b> and an integer <b><i>target</i></b>, return indices of the two numbers such 
         /// that they add up to <b><i>target</i></b>.<br/>
         /// You may assume that each input would have exactly one solution, and you may not use the same element twice.<br/>
-        /// You can return the answer in any order.
+        /// You can return the answer in any order.<br/><br/>
+        /// <b>Example</b>: <br/>
+        /// <code>
+        /// Input: nums = [2,7,11,15], target = 9
+        /// Output: [0,1]
+        /// </code>
+        /// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
         /// </summary>
         /// <param name="_"></param>
         /// <param name="nums">Array of integers</param>
